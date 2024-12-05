b76f757ce04e894099f78a85516f1ade0cde2d6d
Laura <122973125+lauragonzalezmoran@users.noreply.github.com>
2024-12-04 22:00:20 -0600

    Update pulse_oximeter_reader.py

--- pulse_oximeter_reader.py
+++ pulse_oximeter_reader.py
@@ -2,9 +2,19 @@ import max30102
 import hrcalc
 
 
-m = max30102.MAX30102()
-
+def initialize_pulse_oximeter():
+    m = max30102.MAX30102()
+    return m
 # 100 samples are read and used for HR/SpO2 calculation in a single loop
-while True:
+def get_pulse_oximeter_data(m):
     red, ir = m.read_sequential()
     print(hrcalc.calc_hr_and_spo2(ir, red))
+    return hrcalc.calc_hr_and_spo2(ir, red)
+
+
+'''m=initialize_pulse_oximeter()
+oxygen_level, oxygen_level_ok, heart_rate, heart_rate_ok = get_pulse_oximeter_data(m)
+print(oxygen_level)
+print(oxygen_level_ok)
+print(heart_rate)
+print(heart_rate_ok)'''
